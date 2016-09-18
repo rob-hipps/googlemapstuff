8460a425d2af0aac252a25e9e51f7d3afe5bef31
rob-hipps <rob.hipps10@gmail.com>
2016-09-17 20:12:45 -0400

    Cambridge map code

---
+++ cambridgemapmarkerCode.R
@@ -0,0 +1,45 @@
+library(ggmap)
+
+#cambridge crash data
+crashes <- read.csv(file.choose(), header=T)
+crashes$lat <- crashes$Latitude
+crashes$lon <- crashes$Longitude
+
+
+
+#plot data on google map through api in R
+map<-get_map(location='cambridge, ma', zoom=13, maptype='roadmap') 
+
+ggmap(map) +
+    geom_point(data = crashes, aes(x = lon, y = lat, fill = "red", alpha = 0.8,size=(Day.Of.Week)), size = 1, shape = 21) +
+    guides(fill=FALSE, alpha=FALSE, size=FALSE)
+
+
+
+
+
+
+route_df <- route(from = "cambridge, ma",
+                  to = "ny,ny",
+                  structure = "route")
+
+my_map <- get_map("new tork state", zoom = 6)
+
+ggmap(my_map) +
+    geom_path(aes(x = lon, y = lat), color = "red", size = 1.5,
+              data = route_df, lineend = "round")
+
+
+map <- qmap('cambridge, ma', zoom = 13, maptype = 'roadmap')
+#plot the density map
+map + stat_density2d(
+    aes(x = Longitude, y = Latitude, fill = ..level.., alpha = ..level..*2), 
+    size = 2, bins = 5, data = crashes, geom = "polygon") +
+    scale_fill_gradient(low = "black", high = "red")
+
+
+
+plot_ly(crashes, lon = lon, lat = lat, text = Day.Of.Week,
+        marker = list( line = list(width = 0)),
+        color = Day.Of.Week, type = 'scattergeo', locationmode = 'USA-states') %>%
+    layout(title = '2014 US city populations<br>(Click legend to toggle)')
